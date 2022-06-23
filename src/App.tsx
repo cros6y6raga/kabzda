709b6e4c585f00e4c63f4337cfb85eedfe91ed24
cros6y6raga <crosbybraga@yandex.ru>
2022-06-23 21:04:02 +0300

    com1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+    );
 }
+
+function AppTitle() {
+    return (
+        <>This is APP component</>
+    )
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    )
+}
+
+function AccordionTitle() {
+    return (
+        <>
+            <h3>Меню</h3>
+        </>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    )
+}
+
 
 export default App;
