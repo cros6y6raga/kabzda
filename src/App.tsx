3ceefa43ce2a4f2d4a47f30c48ad2f87080600fc
cros6y6raga <crosbybraga@yandex.ru>
2022-06-25 12:24:14 +0300

    com2

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,36 @@
 import React from 'react';
 import './App.css';
+import {Accordion} from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
+    console.log('App rendering')
     return (
         <div>
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
-            <Rating/>
+            <PageTitle title={'This is APP component'}/>
+            <PageTitle title={'My friends'}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion titleValue={'Menu'}/>
+            <Accordion titleValue={'Users'}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
-
-function AppTitle() {
+type PageTitlePropsType = {
+    title: string
+}
+function PageTitle(props:PageTitlePropsType) {
+    console.log('PageTitle rendering')
     return (
-        <>This is APP component</>
+        <h1>{props.title}</h1>
     )
 }
-
-function Rating() {
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    )
-}
-
-function Accordion() {
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
-
-function Star() {
-    return (
-        <div>star</div>
-    )
-}
-
-function AccordionTitle() {
-    return (
-        <>
-            <h3>Меню</h3>
-        </>
-    )
-}
-
-function AccordionBody() {
-    return (
-        <>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </>
-    )
-}
-
 
 export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+type AccordionPropsType = {
+    titleValue: string
+}
+export function Accordion(props:AccordionPropsType) {
+    console.log('Accordion rendering')
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function AccordionTitle(props:any) {
+    console.log('AccordionTitle rendering')
+    return (
+        <>
+            <h3>{props.title}</h3>
+        </>
+    )
+}
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    )
+}
